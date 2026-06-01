5986058c86ca837d4b02864cb0854bd69c037b5d
EObed <obedeffum10@gmail.com>
2026-06-01 16:21:47 +0000

    Created and imported tanstack query provider

---
+++ providers/TanstackQueryProvider.tsx
@@ -0,0 +1,15 @@
+"use client"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+interface IProps{
+    children: React.ReactNode
+}
+
+export default function TanstackQueryProvider({children}: IProps) {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>)
+
+}
